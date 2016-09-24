3a1005a21446012c3874f303a21031ca092d759b
Mitar <mitar.git@tnode.com>
2016-09-24 16:37:35 -0700

    Initial version.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# An example script to run TCP iperf. It uses hard-coded IP address for the server.
+
+NAME='iperf'
+SERVER_IP='10.20.32.10'
+
+docker stop "${NAME}" || true
+sleep 1
+docker rm "${NAME}" || true
+sleep 1
+docker run --detach=true --restart=always --name "${NAME}" --publish "5001:$SERVER_IP:5001/tcp" tozd/iperf
