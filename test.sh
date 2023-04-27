5a9031a39b9316c70ca28afd402a5d50619d0391
Mitar <mitar.git@tnode.com>
2023-04-27 17:39:53 +0200

    Add test.

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Preparing"
+apk add --no-cache iperf || exit 1
+
+echo "Running Docker image"
+docker run -d --name test --rm -p 5001:5001 "${CI_REGISTRY_IMAGE}:${TAG}" || exit 1
+
+echo "Sleeping"
+sleep 10
+
+echo "Testing"
+# We check if there is any comma in succesful CSV output. On errors there is no CSV output.
+iperf --client docker --port 5001 --peer-detect --reportstyle C | grep -q ,
+result=$?
+
+echo "Stopping Docker image"
+docker stop test
+
+exit "$result"
