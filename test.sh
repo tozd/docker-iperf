5511c2e8f8c890b8003f2a11836c428a8a5521e0
Mitar <mitar.git@tnode.com>
2023-04-28 15:27:09 +0200

    Improve script.

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+set -e
+
+cleanup() {
+  echo "Stopping Docker image"
+  docker stop test
+}
+
 echo "Preparing"
-apk add --no-cache iperf || exit 1
+apk add --no-cache iperf
 
 echo "Running Docker image"
-docker run -d --name test --rm -p 5001:5001 "${CI_REGISTRY_IMAGE}:${TAG}" || exit 1
+docker run -d --name test --rm -p 5001:5001 "${CI_REGISTRY_IMAGE}:${TAG}"
+trap cleanup EXIT
 
 echo "Sleeping"
 sleep 10
@@ -12,9 +20,3 @@ sleep 10
 echo "Testing"
 # We check if there is any comma in succesful CSV output. On errors there is no CSV output.
 iperf --client docker --port 5001 --reportstyle C --time 10 | grep -q ,
-result=$?
-
-echo "Stopping Docker image"
-docker stop test || exit 2
-
-exit "$result"
